0c804c4738cdf923361cb5c01628ea0afadf2769
Xcelliti2 <team.bafl@xcelliti.com>
2023-05-02 11:40:55 +0500

    Initializing Retail Skeleton

--- src/Cucumber/TestNGTestRunner.java
+++
@@ -1,8 +0,0 @@
-package Cucumber;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features = "src/Cucumber/Feature/CustomerCreate.feature",glue = "src/Cucumber/stepDefs/stepDefinitions.java", monochrome = true, plugin = {"html:target/cucumber.html"})
-public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-}
