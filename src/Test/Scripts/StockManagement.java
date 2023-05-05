fd2afed9ae29199957c1e8ee476452f667bd7d5f
xcelliti1 <team.bafl@xcelliti.com>
2023-05-05 17:31:04 +0500

    Committing FT + Stop Payment + Stock Management Scripts

--- src/Test/Scripts/StockManagement.java
+++ src/Test/Scripts/StockManagement.java
@@ -2,10 +2,16 @@ package Test.Scripts;
 
 import POM.PageObject;
 import Test.General.BaseClass;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StockManagement extends BaseClass {
 
@@ -22,9 +28,9 @@ public class StockManagement extends BaseClass {
         PageObject.switchFrame(2);
         PageObject.img_Button("New Deal");
 
-        PageObject.textinput_Locator("fieldName:INSTRUMENT.TYPE:1","BC");
-        PageObject.textinput_Locator("fieldName:FROM.SERIAL.NUM:1","123456");
-        PageObject.textinput_Locator("fieldName:TO.SERIAL.NUM:1","123456");
+        PageObject.textinput_Locator("fieldName:INSTRUMENT.TYPE:1","CDR");
+        PageObject.textinput_Locator("fieldName:FROM.SERIAL.NUM:1","444444680");
+        PageObject.textinput_Locator("fieldName:TO.SERIAL.NUM:1","444444685");
         PageObject.textinput_Locator("fieldName:DEBIT.AMOUNT","345");
         PageObject.textinput_Locator("fieldName:INVOICE.NUMBER","55555");
 
@@ -111,8 +117,8 @@ public class StockManagement extends BaseClass {
 
     //Auth will be depended upon stocksReceived
     //Add parameter in @annotation of dependsUponMethod
-    @Test(groups = {"Authorizer"})
-    public void authTheStocks() throws IOException  {
+    @Test(groups = {"Authorizer"},dataProvider = "excelData")
+    public void authTheStocks(Map<String, String> testData) throws IOException  {
 
         PageObject.menu_Dropdown("Customer Services");
         PageObject.menu_Dropdown("Stock Managment");
@@ -121,29 +127,60 @@ public class StockManagement extends BaseClass {
         String HomePage2 = driver.getWindowHandle();
         PageObject.switchToChildWindow();
         //Got the value from DataProvider file
-        PageObject.textinput_Locator("value:1:1:1","FT2235702H4S2Q11");
+        PageObject.textinput_Locator("value:1:1:1",testData.get("Transaction Number"));
         PageObject.find_Button();
         PageObject.form_Link("Authorise Transaction");
-
+        PageObject.img_Button("Authorises a deal");
         //then logout
     }
 
     //DependsOnMethods = {"stocksTransferFromMStoWS","stocksTransferFromWStoMS"}
-    @Test(groups = {"Authorizer"})
-    public void authTheStockTransfers() throws IOException  {
+    @Test(groups = {"Authorizer"},dataProvider = "excelData")
+    public void authTheStockTransfers(Map<String, String> testData) throws IOException  {
 
         PageObject.menu_Dropdown("Customer Services");
         PageObject.menu_Dropdown("Stock Managment");
         PageObject.menu_Dropdown("Authorisation of Stock");
         PageObject.menu_Link("Authorise/Delete Stock Transfer Records ");
+
         String HomePage2 = driver.getWindowHandle();
         PageObject.switchToChildWindow();
+
         //Got the value from DataProvider file
-        PageObject.textinput_Locator("value:1:1:1","BC.001201");
+        PageObject.textinput_Locator("value:1:1:1",testData.get("Transaction Number"));
         PageObject.find_Button();
-        //PageObject.form_Link("Authorise Transaction");
+        PageObject.form_Link("Authorise Transaction");
+        PageObject.img_Button("Authorises a deal");
+
         //Authorise click
         //then logout
     }
 
+
+    @DataProvider(name = "excelData")
+    public Object[][] readExcelData() throws IOException {
+        String FILE_PATH = System.getProperty("user.dir")+"\\Data\\StockManagement_StocksReceived.xlsx";
+        FileInputStream fis = new FileInputStream(FILE_PATH);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheetAt(0); // Assuming data is in the first sheet
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+        Object[][] data = new Object[rowCount - 1][1]; // One column to store the HashMap
+
+        for (int i = 1; i < rowCount; i++) { // Start from row 1 to exclude header row
+            Row row = sheet.getRow(i);
+            Map<String, String> map = new HashMap<String, String>();
+            for (int j = 0; j < colCount; j++) {
+                Cell cell = row.getCell(j);
+                DataFormatter formatter = new DataFormatter();
+                String value = formatter.formatCellValue(cell);
+                map.put(sheet.getRow(0).getCell(j).toString(), value); // Assuming the first row contains column names
+            }
+            data[i - 1][0] = map;
+        }
+
+        workbook.close();
+        fis.close();
+        return data;
+    }
 }
