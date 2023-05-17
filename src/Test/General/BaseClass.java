85f2b6282263116736f4ec47c3c8980faab43157
xcelliti4 <team.bafl@xcelliti.com>
2023-05-17 10:50:52 +0500

    updated work marium

--- src/Test/General/BaseClass.java
+++ src/Test/General/BaseClass.java
@@ -25,8 +25,6 @@ public class BaseClass {
     public static Actions action;
     public static String homePage;
 
-    public static String fileName="";
-
     // Configuration Property File
     public String getProperty(String key) throws IOException {
         //Reading configuration file from the path
@@ -52,6 +50,7 @@ public class BaseClass {
         driver.get("https://172.24.128.50/R22SIT2/servlet/BrowserServlet");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
+
     }
 
     // Configuration Of Edge Driver
@@ -83,11 +82,25 @@ public class BaseClass {
         return System.getProperty("user.dir") + "\\Reports\\" + testCaseName + ".png";
     }
 
+    @BeforeMethod(groups = {"caoInputter"})
+    public void caouserLogin() {
+        chromeConfig();
+        PageObject.signIn("caouser002", "QWer1234");
+        PageObject.switchFrame(1);
+    }
+
+    @BeforeMethod(groups = {"caoInputter"})
+    public void caoauthLogin() {
+        chromeConfig();
+        PageObject.signIn("caoauth002", "QWer1234");
+        PageObject.switchFrame(1);
+    }
+
     @BeforeMethod(groups = {"Inputter"})
     public void inputterLogin() {
         chromeConfig();
 
-        PageObject.signIn("retail006", "QWer12345");
+        PageObject.signIn("retail007", "QWer1234");
 
         PageObject.switchFrame(1);
 
@@ -98,7 +111,6 @@ public class BaseClass {
 
         PageObject.maximizeWindow();
         PageObject.switchFrame(1);
-
     }
 
 
@@ -106,7 +118,7 @@ public class BaseClass {
     public void authorizerLogin() {
         chromeConfig();
 
-        PageObject.signIn("retailauth006", "QWer1234");
+        PageObject.signIn("retailauth007", "QWer1234");
 
         PageObject.switchFrame(1);
 
@@ -116,20 +128,18 @@ public class BaseClass {
 
 
     @AfterMethod(groups = {"Authorizer" , "Inputter"})
-    public void userLogout(){
+    public void userLogout() throws InterruptedException {
+        Thread.sleep(1000);
         this.driver.close();
 
         PageObject.switchToParentWindow(homePage);
-
         PageObject.switchFrame(0);
 
         PageObject.signOff();
-
+        Thread.sleep(1000);
         this.driver.close();
     }
-
 }
-
 
 
 
