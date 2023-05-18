156b4096543df4e0eb670ac10c18dbf9ea6d1f9a
xcelliti5 <sara.hasan@bankalfalah.com>
2023-05-18 15:39:10 +0500

    CDR module

---
+++ src/Test/Scripts/Conventional/CDR_Status.java
@@ -0,0 +1,43 @@
+package Test.Scripts.Conventional;
+
+import POM.PageObject;
+import Test.General.BaseClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class CDR_Status extends BaseClass {
+
+    @Test(groups = {"Inputter"})
+
+    public void CDRStatusStop() throws InterruptedException, IOException {
+
+        PageObject.menu_Dropdown("Remittance/Clearing Officer -Universal Teller");
+        PageObject.menu_Dropdown("Remittance Menu");
+        PageObject.menu_Dropdown("Alfalah Core/Retail Menu ");
+        PageObject.childmenu_Dropdown("Customer Services", 2);
+        PageObject.menu_Dropdown("Call Deposit Receipt- Inputter Menu");
+        PageObject.menu_Dropdown("Call Deposit Receipt Maintenance");
+
+
+        String HomePage2 = driver.getWindowHandle();
+
+        PageObject.menu_Link("Change Status of Instrument ");
+
+        PageObject.switchToChildWindow();
+        PageObject.textinput_Locator("value:1:1:1","CDR.LDA0610547");
+
+        PageObject.find_Button();
+
+        PageObject.maximizeWindow();
+
+        PageObject.form_Link("Change Status");
+
+        Thread.sleep(2000);
+
+        PageObject.commitDeal("CDRStatusStop");
+
+
+}
+
+}
