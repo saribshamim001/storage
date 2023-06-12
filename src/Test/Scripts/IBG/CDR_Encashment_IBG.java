e14ec9e8f0beed753678b7eea5d606acce88b5df
xcelliti5 <sara.hasan@bankalfalah.com>
2023-06-12 16:20:59 +0500

    Sara work

--- src/Test/Scripts/IBG/CDR_Encashment_IBG.java
+++ src/Test/Scripts/IBG/CDR_Encashment_IBG.java
@@ -1,16 +1,77 @@
 package Test.Scripts.IBG;
 
+import POM.PageObject;
 import Test.General.BaseClass;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CDR_Encashment_IBG extends BaseClass {
 
-    @Test(groups = {"IBGInputter"})
+    String FILE_PATH = System.getProperty("user.dir")+"\\Excel Data\\ CDREncashmentinput.xlsx";
 
-    public void CDREncashmentIBG() throws IOException, InterruptedException {
+    @Test(groups = {"IBGInputter"}, dataProvider = "excelDataCDREncashmentinput")
+
+    public void CDREncashmentIBG(Map<String, String> testData) throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Remittance Menu -Universal Teller- IBG");
+        PageObject.menu_Dropdown("Call Deposit Receipt- Inputter Menu");
+
+        PageObject.menu_Dropdown("Call Deposit Receipt Maintenance");
+        PageObject.menu_Link("Call Deposit Receipt Encashment (Within Bank) ");
+
+        PageObject.parentFrame();
+        PageObject.switchFrame(2);
+
+        PageObject.img_Button("New Deal");
+
+        PageObject.textinput_Locator("fieldName:CREDIT.ACCT.NO", testData.get("CREDIT.ACCT.NO"));
+        PageObject.click_Locator("fieldName:CREDIT.THEIR.REF");
+
+        String HomePage2 = PageObject.switchToChildWindow();
+        //PageObject.maximizeWindow();
+        driver.close();
+
+        PageObject.switchToParentWindow(HomePage2);
+        PageObject.switchFrame(2);
+        PageObject.textinput_Locator("fieldName:CREDIT.THEIR.REF", testData.get("CREDIT.THEIR.REF"));
+
+        //PageObject.commitDeal("CDREncashment");
 
 
+    }
+
+    @DataProvider(name = "excelDataCDREncashmentinput")
+    public Object[][] readExcelData1() throws IOException {
+
+        FileInputStream fis = new FileInputStream(FILE_PATH);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheetAt(0); // Assuming data is in the first sheet
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        //rowCount-=2;
+        int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+        Object[][] data = new Object[rowCount - 1][1]; // One column to store the HashMap
+
+        for (int i = 1; i < rowCount; i++) { // Start from row 1 to exclude header row
+            Row row = sheet.getRow(i);
+            Map<String, String> map = new HashMap<String, String>();
+            for (int j = 0; j < colCount; j++) {
+                Cell cell = row.getCell(j);
+                DataFormatter formatter = new DataFormatter();
+                String value = formatter.formatCellValue(cell);
+                map.put(sheet.getRow(0).getCell(j).toString(), value); // Assuming the first row contains column names
+            }
+            data[i - 1][0] = map;
+        }
+
+        workbook.close();
+        fis.close();
+        return data;
     }
 }
