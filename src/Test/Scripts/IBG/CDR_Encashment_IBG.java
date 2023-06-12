de03f8d615c0239a4325c543844dcccb97bde2e0
xcelliti5 <sara.hasan@bankalfalah.com>
2023-06-12 16:26:48 +0500

    Sara work

--- src/Test/Scripts/IBG/CDR_Encashment_IBG.java
+++ src/Test/Scripts/IBG/CDR_Encashment_IBG.java
@@ -74,4 +74,56 @@ public class CDR_Encashment_IBG extends BaseClass {
         fis.close();
         return data;
     }
+
+    @Test(groups = {"Authorizer"})
+
+    public void  CDREncashment_Auth() throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("IBG - Manager Operation Menu");
+
+        PageObject.menu_Dropdown("Core Retail Menu");
+
+        PageObject.menu_Dropdown("Call Deposit Receipt- Authorizer Menu");
+
+        PageObject.menu_Dropdown("Call Deposit Receipt Maintenance Authorize");
+
+        PageObject.menu_Link("Authorization Encashment CDR Instrument- FT ");
+
+        String menu1 = PageObject.switchToChildWindow();
+
+        PageObject.find_Button();
+
+        PageObject.form_Link("Authorize Transaction");
+
+        String menu2 = PageObject.switchToChildWindow();
+        PageObject.img_Button("Authorises a deal");
+
+    }
+
+    @DataProvider(name = "CDREncashmentinput")
+    public Object[][]  CDREncashmentinputinputData() throws IOException {
+        String FILE_PATH = System.getProperty("user.dir")+"\\Data\\CDREncashmentinput.xlsx";
+        FileInputStream fis = new FileInputStream(FILE_PATH);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheetAt(0); // Assuming data is in the first sheet
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+        Object[][] data = new Object[rowCount - 1][1]; // One column to store the HashMap
+
+        for (int i = 1; i < rowCount; i++) { // Start from row 1 to exclude header row
+            Row row = sheet.getRow(i);
+            Map<String, String> map = new HashMap<String, String>();
+            for (int j = 0; j < colCount; j++) {
+                Cell cell = row.getCell(j);
+                DataFormatter formatter = new DataFormatter();
+                String value = formatter.formatCellValue(cell);
+                map.put(sheet.getRow(0).getCell(j).toString(), value); // Assuming the first row contains column names
+            }
+            data[i - 1][0] = map;
+        }
+
+        workbook.close();
+        fis.close();
+        return data;
+    }
 }
