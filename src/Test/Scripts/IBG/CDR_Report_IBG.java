f7047473ef6128d588778054f61d5635ad366fca
xcelliti5 <sara.hasan@bankalfalah.com>
2023-06-12 17:08:39 +0500

    Sara work

--- src/Test/Scripts/IBG/CDR_Report_IBG.java
+++ src/Test/Scripts/IBG/CDR_Report_IBG.java
@@ -1,5 +1,6 @@
 package Test.Scripts.IBG;
 
+import POM.PageObject;
 import Test.General.BaseClass;
 import org.testng.annotations.Test;
 
@@ -10,5 +11,21 @@ public class CDR_Report_IBG extends BaseClass {
     @Test(groups = {"IBGInputter"})
 
     public void CDRReportIBG() throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Remittance Menu -Universal Teller- IBG");
+        PageObject.menu_Dropdown("Call Deposit Receipt- Inputter Menu");
+        PageObject.menu_Dropdown("Call Deposit Receipt Report");
+
+        String HomePage2 = driver.getWindowHandle();
+        PageObject.menu_Link("List of Call Deposit Receipt Instrument ");
+
+        PageObject.switchToChildWindow();
+
+        PageObject.find_Button();
+
+        Thread.sleep(3000);
+
+        PageObject.maximizeWindow();
+
     }
 }
