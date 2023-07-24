54127e21e1d46d4c2d3c72ed5f72f0206692e1ca
w.khan <w.khan@bankalfalah.com>
2023-07-24 12:24:35 +0500

    Added the classes of IBG -Waqas Nadeem Khan 14-June-23

--- src/Test/Scripts/IBG/TDR/Enquiries_IBG.java
+++ src/Test/Scripts/IBG/TDR/Enquiries_IBG.java
@@ -37,4 +37,118 @@ public class Enquiries_IBG extends BaseClass {
             PageObject.find_Button();
 
         }
+
+    @Test(groups = {"InputterTDR_IBG"})
+    public void OtherLoansdisbursedtoday () throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Centralized TDR Menu - IBG");
+        PageObject.childmenu_Dropdown("Enquiries", 1);
+        PageObject.childmenu_Link("Other Loans disbursed today ", 1);
+
+        String menu = PageObject.switchToChildWindow();
+        PageObject.maximizeWindow();
+
+        PageObject.find_Button();
+
+    }
+
+    @Test(groups = {"InputterTDR_IBG"})
+    public void InterestAccrualsOtherLoans  () throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Centralized TDR Menu - IBG");
+        PageObject.childmenu_Dropdown("Enquiries", 1);
+        PageObject.childmenu_Link("Interest Accruals (Other Loans) ", 1);
+
+        String menu = PageObject.switchToChildWindow();
+        PageObject.maximizeWindow();
+
+        PageObject.find_Button();
+
+    }
+
+    @Test(groups = {"InputterTDR_IBG"})
+    public void RepaymentHistoryOtherLoans () throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Centralized TDR Menu - IBG");
+        PageObject.childmenu_Dropdown("Enquiries", 1);
+        PageObject.childmenu_Link("Repayment History (Other Loans) ", 1);
+
+        String menu = PageObject.switchToChildWindow();
+        PageObject.maximizeWindow();
+
+        PageObject.textinput_Locator("value:1:1:1", "LD1419712927");
+        PageObject.find_Button();
+
+    }
+
+    @Test(groups = {"InputterTDR_IBG"})
+    public void LDNewDepositsReceivedToday   () throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Centralized TDR Menu - IBG");
+        PageObject.childmenu_Dropdown("Enquiries", 1);
+        PageObject.childmenu_Link("LD New Deposits Received Today ", 1);
+
+        String menu = PageObject.switchToChildWindow();
+        PageObject.maximizeWindow();
+
+        PageObject.find_Button();
+
+    }
+
+    @Test(groups = {"InputterTDR_IBG"})
+    public void TransactionSchedules   () throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Centralized TDR Menu - IBG");
+        PageObject.childmenu_Dropdown("Enquiries", 1);
+        PageObject.childmenu_Link("Transaction Schedules ", 1);
+
+        String menu = PageObject.switchToChildWindow();
+        PageObject.maximizeWindow();
+
+        PageObject.find_Button();
+
+    }
+
+    @Test(groups = {"InputterTDR_IBG"})
+    public void TransactionSchedulesForward    () throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Centralized TDR Menu - IBG");
+        PageObject.childmenu_Dropdown("Enquiries", 1);
+        PageObject.childmenu_Link("Transaction Schedules Forward ", 1);
+
+        String menu = PageObject.switchToChildWindow();
+        PageObject.maximizeWindow();
+
+        PageObject.find_Button();
+
+    }
+
+    @Test(groups = {"InputterTDR_IBG"})
+    public void AccountEntriesforGivendates      () throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Centralized TDR Menu - IBG");
+        PageObject.childmenu_Dropdown("Enquiries", 1);
+        PageObject.childmenu_Link("Account Entries for Given dates  ", 1);
+
+        String menu = PageObject.switchToChildWindow();
+        PageObject.maximizeWindow();
+
+        PageObject.find_Button();
+
+    }
+
+    @Test(groups = {"InputterTDR_IBG"})
+    public void AccountStmt () throws IOException, InterruptedException {
+
+        PageObject.menu_Dropdown("Centralized TDR Menu - IBG");
+        PageObject.childmenu_Dropdown("Enquiries", 1);
+        PageObject.childmenu_Link("Account Stmt ", 1);
+
+        String menu = PageObject.switchToChildWindow();
+        PageObject.maximizeWindow();
+
+        PageObject.find_Button();
+
+    }
+
     }
