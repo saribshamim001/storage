a4b61295bcdc1a194ec3a73ca5d5fb19d0a72bb3
xcelliti5 <sara.hasan@bankalfalah.com>
2023-08-09 17:40:40 +0500

    Committed Corporate Customer excel file

--- src/Test/Scripts/IBG/BankersChequeReport_IBG.java
+++ src/Test/Scripts/IBG/BankersChequeReport_IBG.java
@@ -2,14 +2,20 @@ package Test.Scripts.IBG;
 
 import POM.PageObject;
 import Test.General.BaseClass;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BankersChequeReport_IBG extends BaseClass {
 
-    @Test(groups = {"Inputter"})
-    public void BankersChequeReport() throws InterruptedException, IOException {
+    @Test(groups = {"inputterIBG"},dataProvider = "BankersChequeReport")
+    public void BankersChequeReport(Map<String, String> testData) throws InterruptedException, IOException {
 
         PageObject.menu_Dropdown("Remittance Menu -Universal Teller- IBG");
         PageObject.childmenu_Dropdown("Cheque- Inputter Menu", 1);
@@ -23,9 +29,41 @@ public class BankersChequeReport_IBG extends BaseClass {
         String menu1 = PageObject.switchToChildWindow();
         PageObject.maximizeWindow();
 
-        PageObject.textinput_Locator("value:1:1:1","20230104");
+        PageObject.textinput_Locator("value:1:1:1",testData.get("value:1:1:1"));//20230104
         PageObject.find_Button();
 
+    }
+
+    String FILE_PATH = "";
+    @DataProvider(name = "BankersChequeReport")
+    public Object[][] dataMethod() throws IOException {
+        String FILE_PATH = System.getProperty("user.dir")+"\\Excel Data\\BankersChequeReport_IBG.xlsx";
+        FileInputStream fis = new FileInputStream(FILE_PATH);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheetAt(0); // Assuming data is in the first sheet
+        int rowCount = sheet.getPhysicalNumberOfRows();
+//        rowCount=6;
+        System.out.println("Row found:  "+rowCount);
+        int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+        System.out.println("Col found:  "+colCount);
+        Object[][] data = new Object[rowCount - 1][1]; // One column to store the HashMap
+
+        for (int i = 1; i < rowCount; i++) { // Start from row 1 to exclude header row
+            Row row = sheet.getRow(i);
+            Map<String, String> map = new HashMap<String, String>();
+            for (int j = 0; j < colCount; j++) {
+                Cell cell = row.getCell(j);
+                DataFormatter formatter = new DataFormatter();
+                String value = formatter.formatCellValue(cell);
+                System.out.println("the value:  "+value);
+                map.put(sheet.getRow(0).getCell(j).toString(), value); // Assuming the first row contains column names
+            }
+            data[i - 1][0] = map;
+        }
+
+        workbook.close();
+        fis.close();
+        return data;
     }
 
     @Test(groups = {"Authorizer"})
